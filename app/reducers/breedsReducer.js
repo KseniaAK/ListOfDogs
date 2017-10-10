1521dc99ce0f093e46af6e015ccb4a3ae3e1c87c
Ksenia <xushenka@Ksenias-MacBook-Pro.local>
2017-10-09 22:50:19 -0700

    capitalize first letter of breed name

--- app/reducers/breedsReducer.js
+++ app/reducers/breedsReducer.js
@@ -3,7 +3,9 @@ import { CHANGE_BREEDS } from '../actions/index'
 export default function(state = [], action) {
   switch(action.type) {
     case CHANGE_BREEDS:
-      const firstTwelveBreeds = Object.keys(action.breeds).map((breed) => breed).slice(0, 12)
+      const firstTwelveBreeds = Object.keys(action.breeds).map((breed) => {
+        return breed.charAt(0).toUpperCase() + breed.slice(1)
+      }).slice(0, 12)
       return firstTwelveBreeds
     
     default:
