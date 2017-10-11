813a3a6f34971c492ec728e4ef637b10f1c9d568
KseniaAK <ksenia.kozh@gmail.com>
2017-10-10 18:22:33 -0700

    add actions to async get pics from dog API

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,8 +1,46 @@
+import axios from 'axios'
+
 export const CHANGE_BREEDS = 'CHANGE_BREEDS'
+export const SELECT_BREED = 'SELECT_BREED'
+export const REQUEST_PICS = 'REQUEST_PICS'
+export const RECEIVE_PICS = 'RECEIVE_PICS'
 
 export const changeBreeds = (breeds) => {
   return {
     type: CHANGE_BREEDS,
     breeds,
+  }
+}
+
+export const selectBreed = (breed) => {
+  return {
+    type: SELECT_BREED,
+    breed
+  }
+}
+
+export const requestPics = (breed) => {
+  return {
+    type: REQUEST_PICS,
+    breed
+  }
+}
+
+export const receivePics = (breed, picsList) => {
+  return {
+    type: RECEIVE_PICS,
+    breed,
+    pics: picsList,
+  }
+}
+
+export const fetchPics = (breed) => {
+  return (dispatch) => {
+    dispatch(requestPics(breed))
+    return axios.get(`https://dog.ceo/api/breed/${breed}/images`)
+      .then(res => {
+        console.log(res.data.message)
+        dispatch(receivePics(breed, res.data.message))
+      })
   }
 }
