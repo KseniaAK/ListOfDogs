7028d9fc4efbc531acd6e2abdd51aad9c58b6b5f
Ksenia <xushenka@Ksenias-MacBook-Pro.local>
2017-10-10 18:25:32 -0700

    add reducers for selecting breed and getting pics

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,10 +2,14 @@ import { combineReducers } from 'redux'
 
 import breedsReducer from './breedsReducer'
 import titleReducer from './titleReducer'
+import singleBreedReducer from './singleBreedReducer'
+import picsReducer from './picsReducer'
 
 const rootReducer = combineReducers({
   breeds: breedsReducer,
   title: titleReducer,
+  selectedBreed: singleBreedReducer,
+  pics: picsReducer,
 });
 
 export default rootReducer
