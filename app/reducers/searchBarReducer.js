2b148e5cb4fe914674127bfca5d7ccc628498546
Ksenia <ksenia.kozh@gmail.com>
2017-10-16 00:02:12 -0700

    rename action property input to searchValue

--- app/reducers/searchBarReducer.js
+++ app/reducers/searchBarReducer.js
@@ -3,7 +3,7 @@ import { READ_USER_INPUT } from '../actions/index'
 export default function(state = '', action) {
   switch(action.type) {
     case READ_USER_INPUT: 
-      return action.input
+      return action.searchValue
 
     default:
       return state
